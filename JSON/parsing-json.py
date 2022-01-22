7b8e4f5bfa2a897748e4b84df172035b0793a004
Nick <nam686@yahoo.com>
2022-01-22 14:33:07 -0600

    format with black

--- JSON/parsing-json.py
+++ JSON/parsing-json.py
@@ -1,6 +1,6 @@
 import json
 
-'''
+"""
 Created by Nick Miles
 nicksnetworklab.com
 
@@ -11,7 +11,8 @@ loads() - Load string. Import JSON data from a string for parsing and manipulati
 dump()  - Write JSON formatted data from Python object into a file
 dumps() - Dump string. Take JSON dictionary data and convert to a serialized string for parsing and manipulating
 
-'''
+"""
+
 
 def main():
 
@@ -22,19 +23,24 @@ def main():
     #  Load in JSON formatted string
     with open("interfaces.json") as data:
         interface_data_string = data.read()
-    
+
     print("Type of interface_data_string:", type(interface_data_string))
     print("Type of interface_data_string:", type(interface_data))
 
     # Convert a JSON formatted string to a python object
-    interfaces_string_object = json.loads(interface_data_string) #uses loads because interfacedata is a string
-    print('Type of interface_data_string after using loads function:', type(interfaces_string_object))
+    interfaces_string_object = json.loads(
+        interface_data_string
+    )  # uses loads because interfacedata is a string
+    print(
+        "Type of interface_data_string after using loads function:",
+        type(interfaces_string_object),
+    )
 
-    '''
+    """
     We can manipulate this JSON data by navigating down the JSON structured and changing the values of the keys. 
     In this examintple, I changed the descriptions of the two GigabitEthernet interfaces, and the IP address 
     information of GigabitEthernet0/0.
-    '''
+    """
     # Editing Descriptions
     print("\nBefore Editing Descriptions")
     print(interface_data["interfaces"][0]["description"])
@@ -57,7 +63,9 @@ def main():
 
     # Saving modified JSON data back to a file using dump
     with open("interfaces_dump.json", "w") as fh:
-        json.dump(interface_data, fh, indent=4) # indent is optional, but helps with improving readability
+        json.dump(
+            interface_data, fh, indent=4
+        )  # indent is optional, but helps with improving readability
 
     # Loop through and print interfaces
     for interface in interface_data["interfaces"]:
